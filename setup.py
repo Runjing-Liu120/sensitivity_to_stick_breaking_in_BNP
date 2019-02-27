06480fc7b5f4647f283846bd4639b03bae6ca878
Runjing <runjing_liu@berkeley.edu>
2019-02-26 20:31:08 -0800

    setup.py to install BNP_modeling

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='SensitivityToBNP',
+    version='0.1dev',
+    packages=['BNP_modeling',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
+
